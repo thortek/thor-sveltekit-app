93538076c54b8c715906a3dd2dcabbe14c059a15
thortek <ta.anderson@gmail.com>
2024-08-26 15:42:44 -0600

    Initial commit

---
+++ src/lib/components/Junk.svelte
@@ -0,0 +1,27 @@
+<script lang="ts">
+    let count = 0
+    let numbers = [1, 2, 3, 4, 5]
+
+    $: if (count >= 10) {
+        alert(`count is dangerously high! ${count}`)
+        count = 9
+    }
+
+    function handleClick() {
+        count += 1
+    }
+
+    function addNumber() {
+        numbers = [...numbers, numbers.length + 1]
+    }
+
+    $: sum = numbers.reduce((a, n) => a + n, 0)
+</script>
+
+<button class="btn variant-filled-primary w-64 m-4" on:click={handleClick}>
+    Clicked {count} {count === 1 ? 'time' : 'times'}
+</button>
+
+<p>{numbers.join(' + ')} = {sum}</p>
+
+<button class="btn variant-outline-primary" on:click={addNumber}>Add a number</button>
---
+++ src/routes/junk/+page.svelte
@@ -0,0 +1,7 @@
+<script lang="ts">
+	import Junk from '$lib/components/Junk.svelte'
+</script>
+
+<Junk/>
+<h1>Hello world!</h1>
+
