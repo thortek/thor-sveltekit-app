2a5fdee7e0a46a48e81f72c1d6c2238e424ae0c6
thortek <ta.anderson@gmail.com>
2024-09-23 15:43:31 -0600

    Added some binding examples

---
+++ src/routes/tutorial/bindings/+page.svelte
@@ -0,0 +1,94 @@
+<script lang="ts">
+	let wantSpam = false
+	let selected: any
+	let answer = ''
+	let scoops = 1
+	let flavors: any = []
+
+	//$: console.log(selected)
+
+	let questions = [
+		{
+			id: 1,
+			text: `Where did you go to school?`
+		},
+		{
+			id: 2,
+			text: `What is your mother's name?`
+		},
+		{
+			id: 3,
+			text: `What is the mascot for SHS?`
+		}
+	]
+
+	function handleSubmit() {
+		alert(`You answered: ${answer} to the question: ${selected.text} with the id of ${selected.id}`)
+	}
+</script>
+
+<div class="card m-4 p-4 w-2/3 mx-auto">
+	<h2>Insecurity questions</h2>
+	<form on:submit|preventDefault={handleSubmit}>
+		<label class="label">
+			<input class="checkbox" type="checkbox" bind:checked={wantSpam} />
+			Yes! Please send me all the spam!
+		</label>
+
+		<select class="select m-2" bind:value={selected} on:change={() => (answer = '')}>
+			{#each questions as question}
+				<option value={question}>{question.text}</option>
+			{/each}
+		</select>
+
+		<input class="input" bind:value={answer} />
+		<button class="btn variant-filled-primary mt-2" disabled={!answer} type="submit">Submit</button>
+	</form>
+</div>
+
+<div class="card m-4 p-4 w-2/3 mx-auto">
+	{#if wantSpam}
+		<p>Thank you for signing up for spam!</p>
+	{:else}
+		<p>You must opt in to continue. If you're not paying, you're the product.</p>
+	{/if}
+</div>
+
+<!-- make a new card with a form element for selecting ice cream scoops and flavours.  Be sure to use the bind:group directive-->
+<div class="card m-4 p-4 w-2/3 mx-auto">
+	<h2>Ice Cream Selection</h2>
+	<form>
+		{#each [1, 2, 3] as number}
+			<label class="label">
+				<input class="radio" type="radio" value={number} bind:group={scoops} />
+				How many scoops? {number}
+				{number === 1 ? 'scoop' : 'scoops'}</label>
+
+			<h2>What flavor?</h2>
+		{/each}
+		<!-- also allow flavor selections -->
+		{#each ['Vanilla', 'Chocolate', 'Strawberry'] as flavor}
+			<label class="label">
+				<input
+					class="checkbox"
+					type="checkbox"
+					name="flavors"
+					value={flavor}
+					bind:group={flavors} />
+				{flavor}
+			</label>
+		{/each}
+
+		{#if flavors.length === 0}
+			<p>Please select at least one flavour</p>
+		{:else if flavors.length > scoops}
+			<p>Can't order more flavours than scoops!</p>
+		{:else}
+			<p>
+				You ordered {scoops}
+				{scoops === 1 ? 'scoop' : 'scoops'}
+				of {flavors}
+			</p>
+		{/if}
+	</form>
+</div>
