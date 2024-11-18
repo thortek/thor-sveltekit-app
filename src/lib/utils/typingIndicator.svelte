675808ec5aba58232a37da699c22fb5320a2d497
thortek <ta.anderson@gmail.com>
2024-11-18 14:53:01 -0700

    Added typingIndicator and readableStreamStore.  Just use them as is!

---
+++ src/lib/utils/typingIndicator.svelte
@@ -0,0 +1,35 @@
+<span class="inline-flex items-center justify-center">
+    <span class="w-1 h-1 mr-1 bg-current rounded-full animate-bounce delay-0" />
+    <span class="w-1 h-1 mr-1 bg-current rounded-full animate-bounce delay-150" />
+    <span class="w-1 h-1 bg-current rounded-full animate-bounce delay-300" />
+</span>
+
+<style>
+    @keyframes bounce {
+        0%, 60%, 100% {
+            transform: translateY(0);
+        }
+        30% {
+            transform: translateY(-4px);
+        }
+    }
+
+    .animate-bounce {
+        animation-name: bounce;
+        animation-duration: 0.75s;
+        animation-timing-function: linear;
+        animation-iteration-count: infinite;
+    }
+
+    .delay-0 {
+        animation-delay: 0s;
+    }
+
+    .delay-150 {
+        animation-delay: 0.15s;
+    }
+
+    .delay-300 {
+        animation-delay: 0.3s;
+    }
+</style>
